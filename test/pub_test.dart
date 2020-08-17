050eee53e987e281dbf49d1eaed0bc0bd1a42c51
xamantra <xamantrx@gmail.com>
2020-08-17 12:46:48 +0800

    change package name

--- test/pub_test.dart
+++ test/pub_test.dart
@@ -1,5 +1,5 @@
-import 'package:pub/src/index.dart';
-import 'package:pub/src/index.dart' as pub;
+import 'package:pub_api/src/index.dart';
+import 'package:pub_api/src/index.dart' as pub;
 import 'package:test/test.dart';
 
 void main() {
